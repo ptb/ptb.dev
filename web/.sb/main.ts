303c3399338b6f0def1e41e40bf9fcf762a91067
Peter T Bosse II <peter@ptb.dev>
2024-06-24 12:15:00 -0400

    Add Storybook support configuration

---
+++ web/.sb/main.ts
@@ -0,0 +1,32 @@
+import { dirname, join } from "node:path"
+
+import type { StorybookConfig } from "@storybook/react-vite"
+
+/**
+  This function is used to resolve the absolute path of a package.
+  It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, "package.json")))
+}
+
+const config: StorybookConfig = {
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions")
+  ],
+  core: {
+    disableTelemetry: true
+  },
+  framework: {
+    /* @ts-expect-error: FrameworkName is string */
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {}
+  },
+  stories: ["../src/**/*.stories.[jt]sx"]
+}
+
+export default config
